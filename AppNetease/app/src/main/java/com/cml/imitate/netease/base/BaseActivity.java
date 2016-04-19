22fb57ca34a45c7a9badd5f8a07de896903071bc
cmlbeliever <851688870@qq.com>
2016-04-19 15:54:36 +0800

    remove all

--- AppNetease/app/src/main/java/com/cml/imitate/netease/base/BaseActivity.java
+++
@@ -1,9 +0,0 @@
-package com.cml.imitate.netease.base;
-
-import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by cmlBeliever on 2016/4/19.
- */
-public class BaseActivity extends AppCompatActivity{
-}
--- AppNetease/commonlib/src/main/java/com/cml/second/app/common/widget/recyclerview/StickyGridLayoutManager.java
+++
@@ -1,7 +0,0 @@
-package com.cml.second.app.common.widget.recyclerview;
-
-/**
- * Created by cmlBeliever on 2016/3/9.
- */
-public class StickyGridLayoutManager  {
-}
