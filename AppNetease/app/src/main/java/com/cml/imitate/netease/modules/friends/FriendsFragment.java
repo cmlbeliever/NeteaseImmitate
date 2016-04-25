eb8959ee0c4bea6ab7367187554fc07325366680
menglin.chen <menglin.chen>
2016-04-25 16:07:39 +0800

    init

---
+++ AppNetease/app/src/main/java/com/cml/imitate/netease/modules/friends/FriendsFragment.java
@@ -0,0 +1,7 @@
+package com.cml.imitate.netease.modules.friends;
+
+/**
+ * Created by cmlBeliever on 2016/4/25.
+ */
+public class FriendsFragment {
+}
---
+++ AppNetease/app/src/main/java/com/cml/imitate/netease/modules/friends/NearbyFragment.java
@@ -0,0 +1,8 @@
+package com.cml.imitate.netease.modules.friends;
+
+/**
+ * Created by cmlBeliever on 2016/4/25.
+ * 附近
+ */
+public class NearbyFragment {
+}
